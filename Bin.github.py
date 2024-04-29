487242ca17568e6cf3c893690b963797a72af82d
Ygor Gabriel Brandão Menezes Lima <163585771+ygorgabrielbml@users.noreply.github.com>
2024-04-29 19:15:05 -0300

    Update Bin.github.py

--- Bin.github.py
+++ Bin.github.py
@@ -1,7 +1,9 @@
-
+from time import sleep
 while True:
     print("Transformador Inteiro para Binário")
+    sleep(1)
     n = int(input("Insira um número inteiro: "))
+    sleep(1)
     x = bin(n)
     if n == 0:
         print("Finalizando o programa!")
